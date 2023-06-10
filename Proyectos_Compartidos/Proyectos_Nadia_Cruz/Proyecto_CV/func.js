44ccba9d2dbae426ec4304dc5b907e29fd398017
Nadia Cruz <112596017+NadiaCruz1@users.noreply.github.com>
2023-06-10 16:09:20 -0300

    Carpeta de proyectos

---
+++ Proyectos_Compartidos/Proyectos_Nadia_Cruz/Proyecto_CV/func.js
@@ -0,0 +1,38 @@
+function GitHub(){
+    var confirmar = confirm('Estas seguro que deseas salir?');
+    if (confirmar){
+      
+      window.location.href = 'https://github.com/NadiaCruz1'
+    
+    }else{
+    }
+}
+
+function mail(){
+        var confirmar = confirm('Estas seguro que deseas salir?');
+        if (confirmar){
+          
+          window.location.href = 'mailto:nadiacruz563@gmail.com '
+        
+        }else{
+        }
+}
+function phone(){
+            var confirmar = confirm('Estas seguro que deseas salir?');
+            if (confirmar){
+              
+              window.location.href = 'tel:+5492604053868'
+            
+            }else{
+            }
+}
+function Linkedin(){
+  var confirmar = confirm('Estas seguro que deseas salir?');
+  if (confirmar){
+    
+    window.location.href = 'https://www.linkedin.com/in/nadia-florencia-cruz-916078255/'
+  
+  }else{
+  }
+}
+            
