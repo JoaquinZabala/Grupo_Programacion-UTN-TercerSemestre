8593f550dacc91e4a5aeb84841d5ca3ebc73c41a
EduMaxiMontes <112594912+EduMaxiMontes@users.noreply.github.com>
2023-06-26 20:45:04 -0300

    Conexion.py

---
+++ Python/Leccion 07 - Clase 10 P.1/Conexion.py
@@ -0,0 +1,32 @@
+from psycopg2 import pool
+from logger_base import log
+# psycopg2 as bd # Esta es otra manera de importar el psycopg2
+import sys
+
+
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'Canas2019'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        pass
+
+    @classmethod
+    def obtenerCursor(cls):
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool()
+
+if __name__ == '__main__':
+    pass
